6fa52d74aa796c16c0c028173939f0fea6360fa4
Orbay A <orbay.altuntoprak@gmail.com>
2024-12-11 20:30:52 +0000

    day07 p2

--- src/solutions/day07.rs
+++ src/solutions/day07.rs
@@ -2,20 +2,23 @@ use std::io::BufRead;
 
 use crate::common::file_reader;
 
-pub fn run(file_path: &str) -> (i64, i64) { (part1(file_path), part2(file_path)) }
+enum Operator {Add, Mul, Concat}
 
-fn part1(file_path: &str) -> i64 {
-    let mut res = 0;
+pub fn run(file_path: &str) -> (i64, i64) {
+    let mut res_two_op = 0;
+    let mut res_three_op = 0;
+
     for line in file_reader(file_path).lines() {   
         let (target, nums) = parse_line(line.unwrap());
-        if check(target, &nums) {
-            res += target;
+        if check(target, nums[0], &nums, 1, &vec![Operator::Add, Operator::Mul]) {
+            res_two_op += target;
+        }
+        if check(target, nums[0], &nums, 1, &vec![Operator::Add, Operator::Mul, Operator::Concat]) {
+            res_three_op += target;
         }
     }
-    return res;
+    return (res_two_op, res_three_op);
 }
-
-fn part2(_file_path: &str) -> i64 { 0 }
 
 fn parse_line(line: String) -> (i64, Vec<i64>) {
     let (fi, se) = line.split_once(": ").unwrap();
@@ -27,21 +30,34 @@ fn parse_line(line: String) -> (i64, Vec<i64>) {
     return (target, nums);
 }
 
-// ENUM OP INSTEAD OF BINARY BS
-fn check(target: i64, nums: &Vec<i64>) -> bool {
-    return (0..1<<(nums.len() - 1) as i64)
-        .into_iter()
-        .any(|encoded_ops| evaluate(&nums, encoded_ops) == target)
+fn check(target: i64, res: i64, nums: &Vec<i64>, i_num: usize, allowed_operators: &Vec<Operator>) -> bool {
+    if i_num == nums.len() {
+        return res == target;
+    }
+    if res > target { // not >= because there might be a bunch of trailing 1s.
+        return false;
+    }
+    for op in allowed_operators {
+        let new_res = calc(res, nums[i_num], op);
+        if new_res.is_some() && check(target, new_res.unwrap(), nums, i_num + 1, allowed_operators) {
+            return true;
+        }
+    }
+    return false;
 }
 
-fn evaluate(nums: &Vec<i64>, operators: i64) -> i64 {
-    let mut res = nums[0];
-    nums.iter().skip(1).enumerate().for_each(|(i, num)| {
-        res = if operators & (1 << i) == 0 {
-            res + num 
-        } else {
-            res * num
-        };
-    });
-    return res;
+fn calc(fi: i64, se: i64, op: &Operator) -> Option<i64> {
+    return match op {
+        Operator::Add => i64::checked_add(fi, se),
+        Operator::Mul => i64::checked_mul(fi, se),
+        Operator::Concat => concat(fi, se),
+    }
+}
+
+fn concat(fi: i64, se: i64) -> Option<i64> {
+    let mut coeff = 10i64;
+    while se >= coeff {
+        coeff *= 10;
+    }
+    return fi.checked_mul(coeff).and_then(|x| x.checked_add(se));
 }
