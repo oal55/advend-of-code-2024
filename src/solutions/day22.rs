a72d15c2b7b18b6c6d11a7acd38957fbd4105dda
Orbay A <orbay.altuntoprak@gmail.com>
2024-12-22 23:06:10 +0000

    day 22

--- src/solutions/day22.rs
+++ src/solutions/day22.rs
@@ -3,59 +3,50 @@ use crate::common::io::file_reader;
 
 const MOD: i64 = 16777216;
 const MASK: i64 = MOD - 1;
+const DIFF_MASK: u32 = 0x00FF_FFFF;
 
 pub fn run(file_path: &str) -> (i64, usize) {
     let seeds = file_reader(file_path).lines().into_iter()
         .map(|l| l.unwrap().parse::<i64>().unwrap())
         .collect::<Vec<_>>();
     
-    let mut seen_seqs = vec![0; 130321]; // 19 ^ 4. "Diffs" have to be in range [-9, 9] -> 19 possible values
+    let mut all_seen_seqs = vec![0; 130321]; // 19 ^ 4. "Diffs" have to be in range [-9, 9] -> 19 possible values
     let mut sum_2k_iter = 0i64;
     for seed in seeds {
-        let (res, seqs, good_indices) = dumber_2000(seed);
-        sum_2k_iter += res;
-        for i in good_indices {
-            seen_seqs[i] += seqs[i];
+        let mut seen_seqs = vec![0; 130321];
+        let mut res = seed;
+        let mut diffs = 0u32;
+        for _ in 0..3 {
+            let next_res = next(res);
+            let diff = ((next_res % 10 - res % 10) + 9) as u8; // this is never negative.
+            diffs = ((DIFF_MASK & diffs) << 8) | (diff as u32);
+            res = next_res;
         }
+
+        for _ in 3..2000 {
+            let next_res = next(res);
+            let diff = ((next_res % 10 - res % 10) + 9) as u8;
+            diffs = ((DIFF_MASK & diffs) << 8) | (diff as u32);
+            res = next_res;
+
+            let i_seq = (
+                (diffs & 0xFF) +
+                19 * ((diffs & 0xFF00) >> 8) +
+                19 * 19 * ((diffs & 0xFF0000) >> 16) +
+                19 * 19 * 19 * ((diffs & 0xFF000000) >> 24)
+            ) as usize;
+
+            if seen_seqs[i_seq] == 0 {
+                all_seen_seqs[i_seq] += (res % 10) as i32;
+                seen_seqs[i_seq] = 1;
+            }
+        }
+
+        sum_2k_iter += res; 
     }
-    let max_bananas = seen_seqs.iter().max().unwrap();
+    let max_bananas = all_seen_seqs.iter().max().unwrap();
 
     (sum_2k_iter, *max_bananas as usize)
-}
-
-fn dumber_2000(start: i64) -> (i64, Vec<i32>, Vec<usize>) {
-    let mask = 0x00_FF_FF_FF; // Fs for fml.
-
-    let mut seen_seqs = vec![-1; 130321];
-    let mut good_indices = Vec::with_capacity(2000);
-    let mut res = start;
-    let mut diffs = 0u32;
-    for _ in 0..3 {
-        let next_res = next(res);
-        let diff = ((next_res % 10 - res % 10) + 9) as u8; // this is never negative.
-        diffs = ((mask & diffs) << 8) | (diff as u32);
-        res = next_res;
-    }
-
-    for _ in 3..2000 {
-        let next_res = next(res);
-        let diff = ((next_res % 10 - res % 10) + 9) as u8;
-        diffs = ((mask & diffs) << 8) | (diff as u32);
-        res = next_res;
-
-        let i_seq = (
-            (diffs & 0xFF) +
-            19 * ((diffs & 0xFF00) >> 8) +
-            19 * 19 * ((diffs & 0xFF0000) >> 16) +
-            19 * 19 * 19 * ((diffs & 0xFF000000) >> 24)
-        ) as usize;
-
-        if seen_seqs[i_seq] == -1 {
-            good_indices.push(i_seq);
-            seen_seqs[i_seq] = (res % 10) as i32;
-        }
-    }
-    (res, seen_seqs, good_indices)
 }
 
 fn next(mut secret: i64) -> i64 {
