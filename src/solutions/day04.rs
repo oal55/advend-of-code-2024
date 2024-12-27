99b613cdbc470130d4d106eb94b8d4a7243d36a7
Orbay A <orbay.altuntoprak@gmail.com>
2024-12-27 22:32:33 +0000

    day 04

--- src/solutions/day04.rs
+++ src/solutions/day04.rs
@@ -1,60 +1,53 @@
-use core::panic;
-use std::iter::zip;
+use std::iter::{zip, once, repeat_n};
 
 use crate::common::io::read_file_as_2d_chars;
 use crate::common::{Grid, Point, Direction};
 
-struct WordFinder { grid: Grid<char> }
-
 pub fn run(file_path: &str) -> (i64, i64) {
     let grid = Grid::new_from_cells(read_file_as_2d_chars(file_path));
-    let word_finder = WordFinder{ grid };
-    (part1(&word_finder), part2(&word_finder))
+    (part1(&grid), part2(&grid))
 }
 
-fn part1(word_finder: &WordFinder) -> i64 {
+fn part1(grid: &Grid<char>) -> i64 {
     let mut res =0;
     let zero = Point::new(0,0);
+    
+    let x_locations = grid.find(&'X');
+    let word = "XMAS".chars().collect::<Vec<_>>();
     for direction in Direction::ALL_DIRS {
-        let diffs :Vec<Point> =  std::iter::once(zero).chain(std::iter::repeat_n(direction, 3)).collect();
-        res += word_finder.count_pattern("XMAS", &diffs);
+        let diffs :Vec<Point> =  once(zero).chain(repeat_n(direction, 3)).collect();
+        res += x_locations.iter()
+            .filter(|&p| matches_at(&grid, *p, &word, &diffs))
+            .count() as i64;
     }
     res
 }
 
-fn part2(word_finder: &WordFinder) -> i64 {
+fn part2(grid: &Grid<char>) -> i64 {
     let diffs = vec![Point::new(0, 0), Point::new(-1, -1), Point::new(0, 2), Point::new(2, 0), Point::new(0, -2)];
-    word_finder.count_pattern("AMMSS", &diffs) + 
-        word_finder.count_pattern("AMSSM", &diffs) + 
-        word_finder.count_pattern("ASSMM", &diffs) + 
-        word_finder.count_pattern("ASMMS", &diffs)
+    let words: Vec<Vec<char>> = vec![
+        "AMMSS".chars().collect(),
+        "AMSSM".chars().collect(),
+        "ASSMM".chars().collect(),
+        "ASMMS".chars().collect(),
+    ];
+    let a_locations = grid.find(&'A');
+    let mut res =0;
+    for word in &words {
+        res += a_locations.iter()
+            .filter(|&p| matches_at(&grid, *p, &word, &diffs))
+            .count() as i64;
+    }
+    res
 }
 
-impl WordFinder {
-    fn count_pattern(&self, word: &str, diffs: &Vec<Point>) -> i64 {
-        if word.len() != diffs.len() {
-            panic!("Brah")
+fn matches_at(grid: &Grid<char>, start_pos: Point, word: &Vec<char>, diffs: &Vec<Point>) -> bool {
+    let mut cur_pos = start_pos;
+    for (&letter, d_pos) in zip(word, diffs) {
+        cur_pos += *d_pos;
+        if !grid.contains(&cur_pos) || letter != *grid.get(&cur_pos) {
+            return false;
         }
-        let chars = word.chars().collect();
-        let mut res = 0;
-        for i in 0..self.grid.num_rows {
-            for j in 0..self.grid.num_cols {
-                if self.matches_at(Point{i, j}, &chars, diffs) {
-                    res += 1;
-                }
-            }
-        }
-        res
     }
-
-    fn matches_at(&self, start_pos: Point, word: &Vec<char>, diffs: &Vec<Point>) -> bool {
-        let mut cur_pos = start_pos;
-        for (&letter, d_pos) in zip(word, diffs) {
-            cur_pos += *d_pos;
-            if !self.grid.contains(&cur_pos) || letter != *self.grid.get(&cur_pos) {
-                return false;
-            }
-        }
-        true
-    }
+    true
 }
