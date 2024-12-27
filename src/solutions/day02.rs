ea9682f527c21ea118a24e84713dd129fbdd8616
Orbay A <orbay.altuntoprak@gmail.com>
2024-12-27 21:24:48 +0000

    day 02

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -1,37 +1,33 @@
-use std::io::BufRead;
+use std::iter::Peekable;
 
-use crate::common::io::file_reader;
+use crate::common::io::read_file;
 
 pub fn run(file_path: &str) -> (i64, i64) {
     let mut num_safe_reports = 0;
     let mut num_safe_after_tweak = 0;
 
-    for r in read_file(file_path) {
-        if is_report_safe(&r) {
+    let reports = read_file(file_path).lines().map(parse_report).collect::<Vec<_>>();
+    for r in reports {
+        if is_report_safe(r.iter().copied().peekable()) {
             num_safe_reports += 1;
-        } else if (0..r.len()).any(|i| is_report_safe(&(r[0..i].iter().copied().chain(r[i+1..].iter().copied()).collect()))) {
+        } else if (0..r.len()).any(|i| is_report_safe(r[0..i].iter().copied().chain(r[i+1..].iter().copied()).peekable())) {
             num_safe_after_tweak += 1;
         }
     }
     (num_safe_reports, num_safe_reports + num_safe_after_tweak)
 }
 
-fn read_file(file_path: &str) -> Vec<Vec<i64>> {
-    file_reader(file_path).lines()
-        .map(Result::unwrap)
-        .map(|line| line.split_ascii_whitespace()
-            .map(|num| num.parse::<i64>().unwrap_or_else(|_| panic!("Cannot parse {num}")))
-            .collect())
-        .collect()
-}
+fn parse_report(l: &str) -> Vec<i32> { l.split(" ").map(|n| n.parse().unwrap()).collect() }
 
-fn is_report_safe(nums: &Vec<i64>) -> bool {
-    if nums[0] > nums[1] {
-        return is_increasing(nums.iter().rev(), nums.iter().rev().skip(1));
+fn is_report_safe(mut nums: Peekable<impl Iterator<Item=i32>>) -> bool {
+    let mut cur = nums.next().unwrap();
+    let order = cur.cmp(nums.peek().unwrap());
+
+    for next_elem in nums {
+        if cur.cmp(&next_elem) != order || cur.abs_diff(next_elem) > 3 {
+            return false
+        }
+        cur = next_elem;
     }
-    is_increasing(nums.iter(), nums.iter().skip(1))
-}
-
-fn is_increasing<'a>(fi: impl Iterator<Item=&'a i64>, se: impl Iterator<Item=&'a i64>) -> bool {
-    fi.zip(se).all(|(sml, big)| sml < big && (big - sml < 4))
+    return true;
 }
